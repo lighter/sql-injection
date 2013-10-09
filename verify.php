d96274217db8f1ae3038d8747daadca106b7c845
lighter <apple.xva@gmail.com>
2013-10-09 17:23:59 +0800

    code modify

--- verify.php
+++ verify.php
@@ -4,7 +4,15 @@ include('db_conn.php');
 $okay     = 0;
 $username = $_POST['user'];
 $pw       = $_POST['pw'];
-$sql      = "select count(*) as ctr from user where u_account='".$username."' and u_pass='". $pw."' limit 1";
+
+// version 1
+// use "' or '1'='1" will pass
+// $sql      = "select count(*) as ctr from user where u_account='".$username."' and u_pass='". $pw."' limit 1";
+
+// version 2
+// use mysql_real_escape_string
+$sql      = "select count(*) as ctr from user where u_account='".mysql_real_escape_string($username)."' and u_pass='".mysql_real_escape_string($pw)."' limit 1";
+
 $result   = mysql_query($sql);
 
 while ($data = mysql_fetch_array($result)){
